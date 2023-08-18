dfc8ce224d2754e6e93469f7436a8ca792166519
Josh Ospina Tafur <josh.999126@gmail.com>
2023-08-18 11:09:25 -0500

    Added Config

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: true,
+  overrides: [
+    {
+      files: ['*.json', '*.md', '*.toml', '*.yml'],
+      options: {
+        useTabs: false,
+      },
+    },
+  ],
+  endOfLine: 'lf',
+}
